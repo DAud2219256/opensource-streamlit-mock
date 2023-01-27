0736264b1630be96f93675679ee0224120b2d001
Douglas Bunker <dbunker@seagen.com>
2023-01-27 09:31:54 -0800

    Add logger module
    Methods are all deadends

---
+++ src/streamlit_mock/streamlit/logger.py
@@ -0,0 +1,18 @@
+def set_log_level(**kwargs):
+    pass
+
+
+def setup_formatter(**kwargs):
+    pass
+
+
+def update_formatter(**kwargs):
+    pass
+
+
+def init_tornado_logs():
+    pass
+
+
+def get_logger(**kwargs):
+    pass
